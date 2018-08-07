627b3a6738c2dce9dae308e399e78a3703713077
ekinguvencoglu <ekinguvencoglu@gmail.com>
2018-08-07 16:20:13 +0300

    Update deneme.js

--- deneme.js
+++ deneme.js
@@ -1,43 +1,46 @@
-'use strict'; 
+"use strict";
 
-const express = require('express');
+const https = require("https");
+const express = require("express");
+var SunCalc = require("suncalc");
 const app = express();
-const fs = require('fs');
-let rawdata = fs.readFileSync('city_list.json');
+const fs = require("fs");
+let rawdata = fs.readFileSync("city_list.json");
 let obj = JSON.parse(rawdata);
+var options = {
+  key: fs.readFileSync("key.pem"),
+  cert: fs.readFileSync("cert.pem"),
+  passphrase: "panda1"
+};
 
-
-function dongu(keyword){
-    var array = new Array();
-for(var i=0; i<obj.length; i++){
+function dongu(keyword) {
+  var array = new Array();
+  for (var i = 0; i < obj.length; i++) {
     var tempObj = obj[i];
+
     var counter = tempObj.name;
-    keyword=keyword.toUpperCase();
-    if(counter.toUpperCase().includes(keyword)){
-        array.push(tempObj);
-        
+    keyword = keyword.toUpperCase();
+    if (counter.toUpperCase().includes(keyword)) {
+        if(array.indexOf())
+      array.push(tempObj);
     }
-}console.log(array);
-return array;
-
+  }
+  console.log(array);
+  return array;
 }
-        
 
-app.get("/getname",function(req,res,next){
-    res.send(obj)
-})
-
-app.get('/', (req, res) => res.send(obj));
-
-
-app.get("/getcity/:keyword",function(req,res){
-var keyword = req.params.keyword;
-dongu(keyword);
-res.send(dongu(keyword));
-
-
-
+app.get("/getname", function(req, res, next) {
+  res.send(obj);
 });
 
+app.get("/", (req, res) => res.send(obj));
 
-app.listen(3000);
+app.get("/getcity/:keyword", function(req, res) {
+  var keyword = req.params.keyword;
+
+  res.send(dongu(keyword));
+});
+
+app.listen(4100);
+
+https.createServer(options, app).listen(2005);
